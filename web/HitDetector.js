84939c942acf39cd825729fd38ad6b5f33983bd4
Trevor Blackwell <tlb@tlb.org>
2013-12-29 20:45:05 -0800

    missing file

---
+++ web/HitDetector.js
@@ -0,0 +1,60 @@
+
+function HitDetector() {
+  var hd = this;
+  hd.hits = [];
+  hd.buttonDown = false;
+  hd.mdX = hd.mdY = null;
+  hd.ctx = null;
+}
+
+HitDetector.prototype.clear = function() {
+  var hd = this;
+  hd.hits = null;
+  hd.ctx = null;
+};
+
+HitDetector.prototype.beginDrawing = function(ctx) {
+  var hd = this;
+  hd.ctx = ctx;
+  hd.hits.length = 0;
+};
+
+HitDetector.prototype.endDrawing = function(ctx) {
+  var hd = this;
+  hd.ctx = null;
+};
+
+HitDetector.prototype.mouseIn = function(l, t, r,b) {
+  var hd = this;
+  return hd.mdX >= l && hd.mdX <= r && hd.mdY >= t && hd.mdY <= b;
+};
+
+
+HitDetector.prototype.add = function(l, t, r, b, draw, click) {
+  var hd = this;
+  hd.hits.push([l, t, r, b, click]);
+  hd.ctx.save();
+  if (hd.buttonDown) {
+    var hilite = hd.mouseIn(l, t, r, b);
+    if (!hilite) hd.ctx.globalAlpha = 0.5;
+    draw();
+  } else {
+    hd.ctx.globalAlpha = 0.5;
+    draw();
+  }
+  hd.ctx.restore();
+};
+
+HitDetector.prototype.find = function(x, y) {
+  var hd = this;
+  var hits = hd.hits;
+  var hitsLen = hits.length;
+  for (var i=0; i<hitsLen; i++) {
+    var hit = hits[i];
+    if (x >= hit[0] && x <= hit[2] &&
+        y >= hit[1] && y <= hit[3]) {
+      return hit[4];
+    }
+  }
+};
+
