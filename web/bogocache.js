2650d5a535abf1258d75e990cbd26e46b1ea1747
Trevor Blackwell <tlb@tlb.org>
2015-03-16 14:53:32 -0700

    Add bogocache

---
+++ web/bogocache.js
@@ -0,0 +1,28 @@
+'use strict';
+var _                   = require('underscore');
+var async               = require('async');
+
+exports.BogoCache = BogoCache;
+
+// timeout in milliseconds
+function BogoCache(timeout) {
+  this.timeout = timeout;
+  this.cache = {};
+};
+
+BogoCache.prototype.get = function(key) {
+  var cKey = '*' + key;
+  var cacheEntry = this.cache[cKey];
+  if (cacheEntry && (Date.now() - cacheEntry.ts) < this.timeout) {
+    return cacheEntry.value;
+  }
+  return undefined;
+};
+
+BogoCache.prototype.set = function(key, value) {
+  var cKey = '*' + key;
+  this.cache[cKey] = {
+    ts: Date.now(),
+    value: value
+  }
+}
