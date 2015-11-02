6b9efab9288421c81f62f407f3ffeb292c5c455e
Trevor Blackwell <tlb@tlb.org>
2015-11-02 11:42:51 -0800

    Add debugging

--- common/parengine.h
+++ common/parengine.h
@@ -17,14 +17,17 @@ struct ParEngine {
   }
   ~ParEngine() {
     while (pending.size() > 0) {
+      eprintf("ParEngine: join\n");
       pending.front().join();
       pending.pop_front();
       readyCv.notify_one();
     }
   }
   void push(thread &&it) {
+    eprintf("ParEngine: start\n");
     unique_lock<mutex> lock(mtx);
     while (pending.size() > 100) {
+      eprintf("ParEngine: wait (pending.size() > 100)\n");
       readyCv.wait(lock);
     }
     pending.push_back(std::move(it));
@@ -48,8 +51,12 @@ struct ParEngineRsv {
       unique_lock<mutex> lock(owner->mtx);
       while (owner->threadsUsed + 1 > owner->threadsAvail ||
              owner->memUsed + min(memNeeded, owner->memAvail) > owner->memAvail) {
+        eprintf("ParEngine: wait (%lu +1 > %lu || %lu + %lu > %lu)\n",
+                (u_long)owner->threadsUsed, (u_long)owner->threadsAvail,
+                (u_long)owner->memUsed, (u_long)memNeeded, owner->memAvail);
         owner->readyCv.wait(lock);
       }
+      eprintf("ParEngine: Allocate %lu\n", (u_long)memNeeded);
       owner->threadsUsed += 1;
       owner->memUsed += memNeeded;
     }
@@ -58,6 +65,7 @@ struct ParEngineRsv {
   ~ParEngineRsv() {
     if (owner) {
       unique_lock<mutex> lock(owner->mtx);
+      eprintf("ParEngine: Release %lu\n", (u_long)memNeeded);
       owner->threadsUsed -= 1;
       owner->memUsed -= memNeeded;
       owner->readyCv.notify_one();
