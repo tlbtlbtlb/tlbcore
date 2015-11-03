e05a66605401cb648bb42e9f1a833963e1661bfe
Trevor Blackwell <tlb@tlb.org>
2015-11-02 16:14:01 -0800

    Better logging

--- common/parengine.h
+++ common/parengine.h
@@ -5,8 +5,10 @@
 
 struct ParEngine {
   
-  ParEngine(size_t threadsAvail = 0, size_t _memAvail = 0)
-    :threadsUsed(0), memUsed(0)
+  ParEngine(size_t _threadsAvail = 0, size_t _memAvail = 0)
+    :threadsUsed(0), threadsAvail(_threadsAvail),
+     memUsed(0), memAvail(_memAvail),
+     verbose(false)
   {
     if (!threadsAvail) {
       threadsAvail = thread::hardware_concurrency();
@@ -16,27 +18,25 @@ struct ParEngine {
     }
   }
   ~ParEngine() {
-    while (pending.size() > 0) {
-      eprintf("ParEngine: join\n");
-      pending.front().join();
-      pending.pop_front();
-      readyCv.notify_one();
-    }
+    finish();
   }
   void push(thread &&it) {
-    eprintf("ParEngine: start\n");
-    unique_lock<mutex> lock(mtx);
-    while (pending.size() > 100) {
-      eprintf("ParEngine: wait (pending.size() > 100)\n");
-      readyCv.wait(lock);
-    }
+    if (verbose) eprintf("ParEngine: start\n");
     pending.push_back(std::move(it));
+  }
+  void finish() {
+    while (pending.size() > 0) {
+      if (verbose) eprintf("ParEngine: join\n");
+      pending.front().join();
+      pending.pop_front();
+    }
   }
   
   mutex mtx;
   condition_variable readyCv;
   size_t threadsUsed, threadsAvail;
   size_t memUsed, memAvail;
+  bool verbose;
 
   deque< thread > pending;
 
@@ -51,12 +51,12 @@ struct ParEngineRsv {
       unique_lock<mutex> lock(owner->mtx);
       while (owner->threadsUsed + 1 > owner->threadsAvail ||
              owner->memUsed + min(memNeeded, owner->memAvail) > owner->memAvail) {
-        eprintf("ParEngine: wait (%lu +1 > %lu || %lu + %lu > %lu)\n",
-                (u_long)owner->threadsUsed, (u_long)owner->threadsAvail,
-                (u_long)owner->memUsed, (u_long)memNeeded, owner->memAvail);
+        if (owner->verbose) eprintf("ParEngine: wait (%lu + 1 > %lu || %lu + %lu > %lu)\n",
+                                    (u_long)owner->threadsUsed, (u_long)owner->threadsAvail,
+                                    (u_long)owner->memUsed, (u_long)memNeeded, owner->memAvail);
         owner->readyCv.wait(lock);
       }
-      eprintf("ParEngine: Allocate %lu\n", (u_long)memNeeded);
+      if (owner->verbose) eprintf("ParEngine: Allocate %lu\n", (u_long)memNeeded);
       owner->threadsUsed += 1;
       owner->memUsed += memNeeded;
     }
@@ -65,7 +65,7 @@ struct ParEngineRsv {
   ~ParEngineRsv() {
     if (owner) {
       unique_lock<mutex> lock(owner->mtx);
-      eprintf("ParEngine: Release %lu\n", (u_long)memNeeded);
+      if (owner->verbose) eprintf("ParEngine: Release %lu\n", (u_long)memNeeded);
       owner->threadsUsed -= 1;
       owner->memUsed -= memNeeded;
       owner->readyCv.notify_one();
