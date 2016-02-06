835f6805244b0e7377d43fdc3ece829dc7c98139
Trevor Blackwell <tlb@tlb.org>
2016-02-05 16:42:02 -0800

    Fix rare bug in js minify

---
+++ web/test_jsmin.js
@@ -0,0 +1,18 @@
+var jsmin               = require('jsmin2');
+
+
+function jsmincomp(a, b) {
+  var b2 = jsmin(a).code;
+  console.log('');
+  console.log(a);
+  console.log(b);
+  console.log(b2);
+  if (b !== b2) throw new Error('Mismatch');
+}
+
+describe('jsmin', function() {
+  it('should work', function() {
+    jsmincomp('this.html(//"old stuff" +\n     "new stuff");', '\nthis.html("new stuff");');
+    jsmincomp('this.html("old stuff" +\n     "new stuff");', '\nthis.html("old stuff"+"new stuff");');
+  });
+});
