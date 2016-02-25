51275ac60b2c1992088b4b8a708754f819f315d4
Trevor Blackwell <tlb@tlb.org>
2016-02-24 16:12:49 -0800

    Fix zmq bind/connect

--- common/zmqwrap.h
+++ common/zmqwrap.h
@@ -13,8 +13,21 @@ template<typename T>
 string bindZmqSocket(T &it, string endpoint) {
   if (it.sock) throw runtime_error("already bound");
   it.sock = new zmqpp::socket(get_process_zmq_context(), zmqpp::socket_type::rep);
-  it.sock->bind("tcp://*:*");
+  it.sock->bind(endpoint.c_str());
   string ret;
   it.sock->get(zmqpp::socket_option::last_endpoint, ret);
+  // zmqpp fails by including a trailing 0 byte in ret.
+  if (ret.size() > 0 && ret.back() == 0) ret.pop_back();
+  return ret;
+}
+
+template<typename T>
+string connectZmqSocket(T &it, string endpoint) {
+  if (it.sock) throw runtime_error("already bound");
+  it.sock = new zmqpp::socket(get_process_zmq_context(), zmqpp::socket_type::rep);
+  it.sock->connect(endpoint.c_str());
+  string ret;
+  it.sock->get(zmqpp::socket_option::last_endpoint, ret);
+  if (ret.size() > 0 && ret.back() == 0) ret.pop_back();
   return ret;
 }
