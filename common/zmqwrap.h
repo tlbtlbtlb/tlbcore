3867378485a82707b5f969656e97186d75aa1c6d
Trevor Blackwell <tlb@tlb.org>
2016-03-07 10:56:33 -0800

    Richer zmqSocket utilities

--- common/zmqwrap.h
+++ common/zmqwrap.h
@@ -10,26 +10,50 @@
 zmqpp::context_t &get_process_zmq_context();
 
 template<typename T>
-string bindZmqSocket(T &it, string endpoint) {
+void createZmqSocket(T &it, string const &sockType)
+{
   if (it.sock) throw runtime_error("already bound");
-  it.sock = new zmqpp::socket(get_process_zmq_context(), zmqpp::socket_type::rep);
-  it.sock->bind(endpoint.c_str());
+  if (sockType == "req") {
+    it.sock = new zmqpp::socket(get_process_zmq_context(),zmqpp::socket_type::req);
+  }
+  else if (sockType == "rep") {
+    it.sock = new zmqpp::socket(get_process_zmq_context(),zmqpp::socket_type::rep);
+  }
+  else {
+    throw runtime_error("createZmqSocket: Unknown sockType");
+  }
+}
+
+template<typename T>
+void closeZmqSocket(T &it)
+{
+  if (it.sock) {
+    it.sock->close();
+    it.sock = nullptr;
+  }
+  it.sockDesc = "(closed)";
+}
+
+template<typename T>
+void labelZmqSocket(T &it)
+{
   string ret;
   it.sock->get(zmqpp::socket_option::last_endpoint, ret);
   // zmqpp fails by including a trailing 0 byte in ret.
   if (ret.size() > 0 && ret.back() == 0) ret.pop_back();
   it.sockDesc = ret;
-  return ret;
 }
 
 template<typename T>
-string connectZmqSocket(T &it, string endpoint) {
-  if (it.sock) throw runtime_error("already bound");
-  it.sock = new zmqpp::socket(get_process_zmq_context(), zmqpp::socket_type::rep);
+void bindZmqSocket(T &it, string endpoint, string sockType) {
+  createZmqSocket(it, sockType);
+  it.sock->bind(endpoint.c_str());
+  labelZmqSocket(it);
+}
+
+template<typename T>
+void connectZmqSocket(T &it, string endpoint, string sockType) {
+  createZmqSocket(it, sockType);
   it.sock->connect(endpoint.c_str());
-  string ret;
-  it.sock->get(zmqpp::socket_option::last_endpoint, ret);
-  if (ret.size() > 0 && ret.back() == 0) ret.pop_back();
-  it.sockDesc = ret;
-  return ret;
+  labelZmqSocket(it);
 }
