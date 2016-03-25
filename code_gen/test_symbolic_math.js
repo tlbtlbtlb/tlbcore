b7696d0396e1be9a0927b5931fe71938be81300e
Trevor Blackwell <tlb@tlb.org>
2016-03-24 17:58:10 -0700

    Fix tests for api change

--- code_gen/test_symbolic_math.js
+++ code_gen/test_symbolic_math.js
@@ -7,19 +7,19 @@ var symbolic_math       = require('./symbolic_math');
 describe('symbolic_math', function() {
   it('should work', function() {
     var c = new symbolic_math.SymbolicContext(null);
-    
+
     var a1 = c.V('double', 'a1');
     var a2 = c.V('double', 'a2');
     var r = c.E('*', a1, a2);
     if (0) console.log(util.inspect(r));
-    var rExpr = c.getCExpr(r);
+    var rExpr = c.getExpr(r);
     assert.strictEqual(rExpr, '(a1 * a2)');
 
     var rWrtA1Expr = c.D(a1, r);
-    assert.strictEqual(c.getCExpr(rWrtA1Expr), 'a2');
+    assert.strictEqual(c.getExpr(rWrtA1Expr), 'a2');
 
     var rWrtA2Expr = c.D(c.V('double', 'a2'), r);
-    assert.strictEqual(c.getCExpr(rWrtA2Expr), 'a1');
+    assert.strictEqual(c.getExpr(rWrtA2Expr), 'a1');
 
     var rImm = c.getImm(r, {a1: 2, a2: 3});
     assert.strictEqual(rImm, 6);
@@ -30,40 +30,39 @@ describe('symbolic_math', function() {
 describe('symbolic_math', function() {
   it('matrices should work', function() {
     var c = new symbolic_math.SymbolicContext(null);
-    
+
     var a = c.V('double', 'a');
     var az = c.E('mat44RotationZ', a);
-    console.log(c.getCExpr(az));
+    console.log(c.getExpr(az));
     var b = c.V('arma::mat44', [1, 0, 0, 0,
-                                0, 1, 0, 0, 
-                                0, 0, 1, 0, 
+                                0, 1, 0, 0,
+                                0, 0, 1, 0,
                                 0, 0, 0, 1]);
     var r = c.E('*', az, b);
-    console.log(c.getCExpr(r));
+    console.log(c.getExpr(r));
   });
 });
-    
+
 
 describe('symbolic_math', function() {
   it('should emit correct C code', function() {
     var c = new symbolic_math.SymbolicContext(null);
-    
+
     var a = c.V('double', 'body.joints.rht');
     var az = c.E('mat44RotationZ', a);
     var x = c.C('arma::mat44', [1, 0, 0, 0,
-                                0, 1, 0, 0, 
-                                0, 0, 1, 0, 
+                                0, 1, 0, 0,
+                                0, 0, 1, 0,
                                 0, 0, 0, 1]);
     var y = c.C('arma::mat44', [0, 1, 0, 0,
-                                1, 0, 0, 0, 
-                                0, 0, 1, 0, 
+                                1, 0, 0, 0,
+                                0, 0, 1, 0,
                                 0, 0, 0, 1]);
-    c.A('body.segments.rht', c.E('+', 
-                                 c.E('*', az, x), 
-                                 c.E('+', 
+    c.A('body.segments.rht', c.E('+',
+                                 c.E('*', az, x),
+                                 c.E('+',
                                      c.E('*', az, y),
                                      c.E('*', az, x))));
-    c.emitCpp(console.log, function(name) { return true ;});
+    c.emitCode(console.log, function(name) { return true ;});
   });
 });
-    
