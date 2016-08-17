022f7170eef43a40abfff34badbe43df4ed9686a
Trevor Blackwell <tlb@tlb.org>
2016-08-16 20:01:19 -0700

    nodeasync tweaks

--- nodebase/nodeasync.cc
+++ nodebase/nodeasync.cc
@@ -5,79 +5,130 @@
 #include <mutex>
 #include <uv.h>
 
+
 struct AsyncEventQueueImpl {
 
-  explicit AsyncEventQueueImpl(uv_loop_t *loop, Local<Function> _onMessage);
+  AsyncEventQueueImpl(AsyncCallbacks *_owner);
   ~AsyncEventQueueImpl();
 
-  void push(jsonstr const &it);
+  void start();
+  void push(string const &eventName, jsonstr const &it);
+  void deliver();
+  void on(string const &eventName, Local<Value> cb);
 
+  AsyncCallbacks *owner;
   uv_async_t uva;
   std::mutex qMutex;
-  deque<jsonstr> q;
-  Persistent<Function> onMessage;
+  deque< pair<string, jsonstr> > q;
+  std::unordered_map< string, vector< shared_ptr< Persistent<Function> > > > nameToCbs;
 };
+
+
+
+AsyncCallbacks::AsyncCallbacks()
+{
+}
+
+AsyncCallbacks::~AsyncCallbacks()
+{
+}
 
 void AsyncCallbacks::emitraw(string const &eventName, jsonstr const &it)
 {
-  shared_ptr<AsyncEventQueueImpl> impl = impls[eventName];
   if (impl) {
-    impl->push(it);
+    impl->push(eventName, it);
   }
 }
 
+void AsyncCallbacks::on(string const &eventName, Local<Value> _onMessage)
+{
+  call_once(implInitOnce, [this]() {
+    impl = make_shared<AsyncEventQueueImpl>(this);
+    impl->start();
+  });
+  impl->on(eventName, _onMessage);
+}
 
-AsyncEventQueueImpl::AsyncEventQueueImpl(uv_loop_t *loop, Local<Function> _onMessage)
+
+// -----------------------
+
+AsyncEventQueueImpl::AsyncEventQueueImpl(AsyncCallbacks *_owner)
+  :owner(_owner)
 {
   if (0) eprintf("AsyncEventQueueImpl constructor\n");
-  uva.data = (void *)this;
-  onMessage.Reset(v8::Isolate::GetCurrent(), _onMessage);
-
-	uv_async_init(loop, &uva, [](uv_async_t* uva1) {
-    /*
-      This part gets called from the uv event loop, when it should be OK to call v8 ops.
-    */
-    Isolate *isolate = v8::Isolate::GetCurrent();
-    HandleScope scope(isolate);
-		auto self = reinterpret_cast<AsyncEventQueueImpl*>(uva1->data);
-
-    Local<Function> onMessageLocal = Local<Function>::New(isolate, self->onMessage);
-    Local<Value> recvLocal = Undefined(isolate);
-
-    while (1) {
-      unique_lock<mutex> lock(self->qMutex);
-      if (self->q.empty()) break;
-      jsonstr msg = self->q.front();
-      self->q.pop_front();
-      lock.unlock();
-      // WRITEME: should we handle blobs somewhere?
-      Local<Value> jsMsg = convJsonstrToJs(isolate, msg);
-      onMessageLocal->Call(recvLocal, 1, &jsMsg);
-    }
-  });
-
-  uv_unref((uv_handle_t *)&uva);
+  uva.data = nullptr;
 }
 
 AsyncEventQueueImpl::~AsyncEventQueueImpl()
 {
-  if (0) eprintf("AsyncEventQueueImpl destructor\n");
-  uv_close((uv_handle_t *)&uva, [](uv_handle_t *uva1) {
-    if (0) eprintf("AsyncEventQueueImpl destructor close callback\n");
-    // ???
-  });
+  if (1) eprintf("AsyncEventQueueImpl destructor\n");
+  if (uva.data) {
+    uv_close((uv_handle_t *)&uva, [](uv_handle_t *uva1) {
+      if (1) eprintf("AsyncEventQueueImpl destructor close callback\n");
+      // ???
+    });
+  }
+  owner = nullptr;
 }
 
-void AsyncEventQueueImpl::push(jsonstr const &json)
+void AsyncEventQueueImpl::start()
+{
+  assert(uva.data == nullptr);
+
+  uva.data = (void *)this;
+  uv_loop_t *loop = uv_default_loop();
+
+  uv_async_init(loop, &uva, [](uv_async_t* uva1) {
+    auto self = reinterpret_cast<AsyncEventQueueImpl*>(uva1->data);
+    self->deliver();
+  });
+  uv_unref((uv_handle_t *)&uva);
+}
+
+void AsyncEventQueueImpl::deliver()
+{
+  /*
+    This part gets called from the uv event loop, when it should be OK to call v8 ops.
+  */
+  Isolate *isolate = v8::Isolate::GetCurrent();
+  HandleScope scope(isolate);
+
+  Local<Value> recvLocal = Undefined(isolate);
+
+  while (1) {
+    unique_lock<mutex> lock(qMutex);
+    if (q.empty()) break;
+    auto &msg = q.front();
+    auto &cbs = nameToCbs[msg.first];
+    if (!cbs.size()) {
+      // shortcut to avoid even json-decoding when no callbacks
+      q.pop_front();
+      continue;
+    }
+    Local<Value> jsMsg = convJsonstrToJs(isolate, msg.second);
+    q.pop_front(); // Can't use msg after this
+    lock.unlock();
+
+    for (auto &cb : cbs) {
+      Local<Function> cbLocal = Local<Function>::New(isolate, *cb);
+      // WRITEME: should we handle blobs somewhere?
+      cbLocal->Call(recvLocal, 1, &jsMsg);
+    }
+  }
+}
+
+void AsyncEventQueueImpl::on(string const &eventName, Local<Value> cb)
+{
+  shared_ptr< Persistent<Function> > cbPersistent = make_shared< Persistent<Function> >();
+  cbPersistent->Reset(v8::Isolate::GetCurrent(), Local<Function>::Cast(cb));
+  unique_lock<mutex> lock(qMutex);
+  nameToCbs[eventName].push_back(cbPersistent);
+}
+
+void AsyncEventQueueImpl::push(string const &eventName, jsonstr const &json)
 {
   unique_lock<mutex> lock(qMutex);
-  q.push_back(json);
+  q.push_back(make_pair(eventName, json));
+  lock.unlock();
   uv_async_send(&uva);
-}
-
-
-
-void asyncCallbacksSet(AsyncCallbacks &it, string const &eventName, uv_loop_t *loop, Local<Value> _onMessage)
-{
-  it.impls[eventName] = make_shared<AsyncEventQueueImpl>(loop, Local<Function>::Cast(_onMessage));
 }
--- nodebase/nodeasync.h
+++ nodebase/nodeasync.h
@@ -1,12 +1,30 @@
 #pragma once
 
+/*
+  This helps make callbacks to JS code, triggerable from C++.
+  You can call emit(...) from any thread.
+  To add callbacks, call asyncCallbacksSet (defined in nodeasync_jsWrap.h)
+*/
+
+namespace v8 {
+  class Value;
+  template<typename T> class Local;
+};
+
 struct AsyncCallbacks {
-  unordered_map<string, shared_ptr<struct AsyncEventQueueImpl> > impls;
+  AsyncCallbacks();
+  ~AsyncCallbacks();
+  AsyncCallbacks(AsyncCallbacks const &) = delete; // not sure how this should work
+
+  std::once_flag implInitOnce;
+  shared_ptr<struct AsyncEventQueueImpl> impl;
 
   template<typename T>
   void emit(string const &eventName, T const &it) {
     emitraw(eventName, asJson(it));
   }
   void emitraw(string const &eventName, jsonstr const &it);
+
+  void on(string const &eventName, v8::Local<v8::Value> _onMessage);
 
 };
