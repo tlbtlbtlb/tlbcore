8c4a0348d591c548324b03c8b0ad4a2d8623f252
Trevor Blackwell <tlb@tlb.org>
2018-03-12 20:53:34 -0700

    Speed and portability optimizations

--- common/std_headers.h
+++ common/std_headers.h
@@ -1,41 +1,39 @@
 //-*-C++-*-
-#ifndef _TLBCORE_STD_HEADERS_H
-#define _TLBCORE_STD_HEADERS_H
-
+#pragma once
 
 #if defined(__AVR32__)
 
-#ifndef __EMBEDDED__
-#define __EMBEDDED__
-#endif
+#  if !defined(__EMBEDDED__)
+#    define __EMBEDDED__
+#  endif
 
-#include <compiler.h>
-#include <string.h>
-#include <machine/endian.h>
-#include <stdint.h>
+#  include <compiler.h>
+#  include <string.h>
+#  include <machine/endian.h>
+#  include <stdint.h>
 
 #elif defined(__AVR__)
 
-#ifndef __EMBEDDED__
-#define __EMBEDDED__
-#endif
+#  if !defined(__EMBEDDED__)
+#    define __EMBEDDED__
+#  endif
 
-#include <avr/io.h>
-#include <avr/pgmspace.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <avr/eeprom.h>
-#include <math.h>
+#  include <avr/io.h>
+#  include <avr/pgmspace.h>
+#  include <avr/interrupt.h>
+#  include <util/delay.h>
+#  include <avr/eeprom.h>
+#  include <math.h>
 
-#include "atmelstuff/iobits.h"
+#  include "atmelstuff/iobits.h"
 
 #elif defined(__EMBEDDED__)
 
 #  include <string.h>
 
-#else
+#else // not embedded
 
-#  ifdef __cplusplus
+#  if defined(__cplusplus)
 #    include <cstdlib>
 #    include <cstdio>
 #    include <cassert>
@@ -54,73 +52,75 @@
 #  define ffs BOGUS_FFS
 #  include <string.h>
 #  undef ffs
+
+
 #  include <time.h>
-#if !defined(WIN32)
-#  include <sys/time.h>
-#endif
+#  if !defined(WIN32)
+#    include <sys/time.h>
+#  endif
+
 #  include <ctype.h>
-#  ifdef __FreeBSD__
+#  if defined(__FreeBSD__)
 #    include <ieeefp.h>
 #  endif
 #  include <sys/param.h>
 #  include <errno.h>
 #  include <fcntl.h>
 #  include <float.h>
-#  include <stdarg.h>
+#  if !defined(__cplusplus)
+#    include <stdarg.h>
+#  endif
+#endif // not embedded
+
+#if defined(__cplusplus)
+#  include <algorithm>
+#  include <vector>
+#  include <deque>
+#  include <numeric>
+#  include <list>
+#  include <string>
+#  include <map>
+#  include <unordered_map>
+#  include <set>
+#  include <array>
+#  include <iostream>
+#  include <iomanip>
+#  include <fstream>
+#  include <iterator>
+#  include <complex>
+#  include <memory>
+#  include <typeinfo>
+#  include <limits>
+#  include <stdexcept>
+using namespace std;
 #endif
 
-#  ifdef __cplusplus
-#    include <algorithm>
-#    include <vector>
-#    include <deque>
-#    include <numeric>
-#    include <list>
-#    include <string>
-#    include <map>
-#    include <unordered_map>
-#    include <set>
-#    include <array>
-#    include <iostream>
-#    include <iomanip>
-#    include <fstream>
-#    include <iterator>
-#    include <complex>
-#    include <memory>
-#    include <typeinfo>
-#    include <limits>
-#    include <stdexcept>
-using namespace std;
-#  endif
 
 
 
-
-#ifndef __EMBEDDED__
+#if !defined(__EMBEDDED__)
 typedef size_t S;
 typedef double R;
 typedef float F;
 #endif
 
-#ifndef _COMPILER_H_
+#if !defined(_COMPILER_H_)
 // For compatability with AVR32-land
 
-#ifdef __AVR__
+#  if defined(__AVR__) // 8 bit
 typedef long int S32;
 typedef unsigned long int U32;
-#else
+#  elif defined(__x86_64) // 64 bit
 typedef int S32;
 typedef unsigned int U32;
-#endif
-
-#ifndef __AVR__
-#ifdef __x86_64
 typedef long long int S64;
 typedef unsigned long long int U64;
-#else
+#  else // 32 bit
+typedef int S32;
+typedef unsigned int U32;
 typedef long long int S64;
 typedef unsigned long long int U64;
-#endif
-#endif
+#  endif
 
 typedef short int S16;
 typedef unsigned short int U16;
@@ -128,56 +128,40 @@ typedef signed char S8;
 typedef unsigned char U8;
 typedef unsigned char Bool;
 
-#  ifndef __cplusplus
+#  if !defined(__cplusplus)
 static __inline int min(int a, int b) { if (a<b) return a; else return b; }
 static __inline int max(int a, int b) { if (a>b) return a; else return b; }
 #  endif
 
+#endif // compiler_h
+
+#if defined(__EMBEDDED__)
+
+#  if !defined(M_PI)
+#    define M_PI 3.1415926535897931
+#    define M_2PI 6.2831853071795862
+#    define M_PI_2 1.5707963267948966
+#    define M_PI_4 0.78539816339744828
+#    define M_2_PI 0.63661977236758138
+#    define M_4_PI 1.2732395447351628
+#    define M_E 2.7182818284590451
+#  endif
+
 #endif
 
-#ifdef __EMBEDDED__
-
-#ifndef M_PI
-#  define M_2PI 6.2831853071795862
-#  define M_PI 3.1415926535897931
-#  define M_PI_2 1.5707963267948966
-#  define M_PI_4 0.78539816339744828
-#  define M_2_PI 0.63661977236758138
-#  define M_4_PI 1.2732395447351628
-#  define M_E 2.7182818284590451
-#endif
-
-#endif
-
-#ifdef __APPLE__
-typedef unsigned long long uint64_t;
-#endif
-
-#ifdef __cplusplus
-#if defined(__GNUC__) || defined(WIN32)
+#if defined(__cplusplus)
 typedef complex< double > complexd;
 typedef complex< float > complexf;
 #endif
-#endif
 
-#if !defined(__cplusplus) && !defined(__OBJC__) && !defined(__ARM__) && !defined(__AVR32__)
-typedef unsigned char bool;
-#endif
-
-#ifdef __EMBEDDED__
-#else
+#if !defined(__EMBEDDED__)
 #  include "./hacks.h"
 
-#ifdef __cplusplus
-#  include "./packetbuf.h"
-#  include "../numerical/numerical.h"
-#endif
+#  if defined(__cplusplus)
+#    include "./packetbuf.h"
+#    include "../numerical/numerical.h"
+#  endif
 
 #  include "./host_debug.h"
 #  include "./host_profts.h"
-#endif
-
-
-
-
 #endif
