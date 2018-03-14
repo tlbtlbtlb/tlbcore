a123b7a9036c488193996c079dbcef6d29b0bfb4
Trevor Blackwell <tlb@tlb.org>
2018-03-13 20:36:24 -0700

    Tweak module loading

---
+++ web/vjs_jquery_ui.js
@@ -0,0 +1,9 @@
+'use strict';
+
+window.$ = window.jQuery = require('jquery');
+require('jquery-ui');
+require('jquery-ui/ui/widgets/tabs');
+require('jquery-ui/ui/unique-id');
+require('jquery-ui/ui/safe-active-element');
+require('jquery-ui/ui/keycode');
+require('jquery-contextmenu');
